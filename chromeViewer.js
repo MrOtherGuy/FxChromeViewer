19bda142c217a0a92b2b78784000ee7ccfb6bfc4
MrOtherGuy <jastekken@outlook.com>
2019-01-02 18:05:23 +0200

    Add JSON output option and change input parameter style

--- chromeViewer.js
+++ chromeViewer.js
@@ -1,8 +1,13 @@
-/*  */
-(new(function(d){
-  const MAX_DEPTH = d || 1;
+(new(function(){
+  let MAX_DEPTH = 1;
+  const TAB_LOAD_ARGS = {relatedToCurrent:false,inBackground:false,triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal()};
   this.state = {depth:0};
   this.output = { value: "" };
+  this.loadBlob = function(mimetype){
+    let blob = URL.createObjectURL(new Blob([this.output.value],{type:mimetype}));
+    gBrowser.loadOneTab(blob,TAB_LOAD_ARGS);
+   setTimeout(()=>(URL.revokeObjectURL(blob)),1000);
+  };
   this.formatLine = (n) => ("\n"+("| ").repeat(n)+"|_");
   this.stringifyHTML = function(node){
     let s = `<span style="color:#20B2AA">${node.tagName}</span><span style="color:#CD5C5C">${(node.id ? "#" + node.id : "")}</span><span style="color:#808000">${(node.classList[0] ? "." + node.classList[0] : "")}${(node.classList[1] ? "." + node.classList[1] : "")}</span>`;
@@ -12,6 +17,7 @@
     let s = `${node.tagName}${(node.id ? "#" + node.id : "")}${(node.classList[0] ? "." + node.classList[0] : "")}${(node.classList[1] ? "." + node.classList[1] : "")}`;
     return s
   };
+  this.stringifyJSON = function(node){ return `"${this.stringify(node)}"`};
   this.parseNode = function(parent,toString){
     this.state.depth++;
     for(let node of parent.children){
@@ -20,7 +26,17 @@
     }
     this.state.depth--
   };
-  this.print = function(selector){
+  this.JSONencode = function(parent){
+    this.state.depth++;
+    for(let node of parent.children){
+      this.output.value += `\n${("  ").repeat(this.state.depth)}${this.stringifyJSON(node)}:{`;
+      this.state.depth < MAX_DEPTH && this.JSONencode(node);
+      this.output.value += "}";
+      this.output.value += node.nextSibling === null ? "\n" : ",";
+    }
+    this.state.depth--
+  };
+  this.createString = function(selector){
     this.output.value = "";
     let node = document.querySelector(selector);
     if(!node){
@@ -31,7 +47,7 @@
     return this.output.value;
   };
   this.createHTML = function(selector){
-    this.output.value = "<html><body><pre><code>";
+    this.output.value = "<html><body style='background:rgb(50,50,50)'><pre><code>";
     let node = document.querySelector(selector);
     if(!node){
       return null
@@ -39,9 +55,45 @@
     this.output.value += this.stringifyHTML(node);
     this.parseNode(node,this.stringifyHTML);
     this.output.value += "</code></pre></body></html>";
-    let args = {relatedToCurrent:false,inBackground:false,triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal()};
-    let blob = URL.createObjectURL(new Blob([this.output.value],{type:"text/html;charset=utf-8"}));
-    gBrowser.loadOneTab(blob,args);
-    URL.revokeObjectURL(blob);
+    this.loadBlob("text/html;charset=utf-8");
   }
-})(6)).createHTML("#navigator-toolbox");
+  this.createJSON = function(selector){
+    this.output.value = "{\n";
+    let node = document.querySelector(selector);
+    if(!node){
+      return null
+    }
+    this.output.value += this.stringifyJSON(node) + ":{";
+    this.JSONencode(node);
+    this.output.value += "}\n}";
+    this.loadBlob("application/json;charset=utf-8");
+    
+  }
+  this.create = function(properties){
+    if(properties.depth){
+      MAX_DEPTH = Math.max(Math.min(properties.depth|0,12),1); // 12 is sane maximum allowed
+    }
+    let output = null;
+    switch(properties.type){
+      case "HTML":
+        this.createHTML(properties.parent);
+        break;
+      case "JSON":
+        this.createJSON(properties.parent);
+        break;
+      case "STRING":
+       output = this.createString(properties.parent);
+        break;
+      default:
+        return null
+    }
+    return output
+  }
+})())
+.create(
+  {
+    type:"JSON",
+    parent:"#navigator-toolbox",
+    depth:6
+  }
+);
